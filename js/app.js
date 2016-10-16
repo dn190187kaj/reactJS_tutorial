0079150942138a874a5d3b636061e73e5aac27c0
Kondratev Aleksandr <dn190187kaj@gmail.com>
2016-10-17 00:56:20 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -1,20 +1,69 @@
-var Comments = React.createClass({
+var my_news = [
+		{
+			author: 'Саша Печкин',
+			text: 'В четверг, четвертого числа...'
+		},
+		{
+			author: 'Просто Вася',
+			text: 'Считаю что доллар должен стоить 35 рублей...'
+		},
+		{
+			author: 'Гость',
+			text: 'Бесплатно. Скачать. Лучший сайт ...'
+		},
+		{
+			author: 'Саша Печкин',
+			text: 'В четверг, четвертого числа...'
+		},
+		{
+			author: 'Просто Вася',
+			text: 'Считаю что доллар должен стоить 35 рублей...'
+		},
+		{
+			author: 'Гость',
+			text: 'Бесплатно. Скачать. Лучший сайт ...'
+		}
+]
+
+
+var Article = React.createClass({
 	render: function(){
+		var author = this.props.data.author;
+		var text = this.props.data.text;
+
 		return (
-			<div className="comments">
-				Нет новостей, комментировать нечего.
-			</div>
-		);
+			<div className="article">
+				<p className="news_author">{author}</p>
+				<p className="news_text">{text}</p>
+			</div>	
+		)
 	}
 })
 
 var News = React.createClass({
 	render: function(){
+		var data = this.props.data;
+		var newsTemplate;
+
+		if (data.length > 0){
+				newsTemplate = data.map(function(item, index){
+					return (
+						<div key={index}>
+							<Article data={item} />
+						</div>
+						)
+				})
+
+			} else {
+				newsTemplate = <p>К сожалению новостей нет</p>
+			}
+
 		return (
 			<div className="news">
-				К сожалению, новостей нет.
+				{newsTemplate}
+				<strong className={'news_count ' + (data.length > 0 ? '' : 'none')}>Всего новосте: {data.length}</strong>
 			</div>
-		);
+		);	
 	}
 })
 
@@ -22,9 +71,8 @@ var App = React.createClass({
 	render: function(){
 		return (
 			<div className="app">
-				Всем привет, я компонент App! Я умею отображать новости.
-				<News />
-				<Comments />
+				<h3>Новости</h3>
+				<News data={my_news}/>    {/*Комментарий*/}
 			</div>
 			);
 	}
